784aaabc20b5c31418673684962f2940774706f0
ArefShojaei <arefshojaei82dev@gmail.com>
2024-12-25 17:22:31 +0330

    ADD: add selector unit test

---
+++ tests/unit/selector.test.js
@@ -0,0 +1,49 @@
+import { JSDOM } from "jsdom";
+import Selector from "../../src/utils/selector.js";
+
+describe("Element tests", () => {
+    it("should protect to create instance of class", () => {
+        expect(() => new Selector).toThrow();
+    });
+
+    it("should get all anchor elements", () => {
+        const dom = new JSDOM(`
+            <html>
+                <head></head>
+                <body>
+                    <a href='/'>Home page</a>
+                    <a href='/products'>Products page</a>
+                    <a href='/blog'>Blog page</a>
+                </body>
+            </html>
+        `);
+
+        const anchorElement = "a";
+
+        const links = Selector.findAll(anchorElement, dom.window.document).getElements();
+
+
+        expect(typeof links).toBe("object")
+    });
+
+    it("should get title of link", () => {
+        const dom = new JSDOM(`
+            <html>
+                <head></head>
+                <body>
+                    <a href='/'>Home page</a>
+                    <a href='/products'>Products page</a>
+                    <a href='/blog'>Blog page</a>
+                </body>
+            </html>
+        `);
+
+        const anchorElement = "a";
+
+        Selector.findAll(anchorElement, dom.window.document).each(anchor => {
+            const link = anchor.getAttribute("href")
+
+            expect(typeof link).toBe("string")
+        })
+    })
+});
