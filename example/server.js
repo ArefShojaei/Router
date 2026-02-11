d73e35e6e92185cd4f4607029bdc147d4f63d3b8
ArefShojaei <arefshojaei82@gmail.com>
2026-02-11 19:19:46 +0330

    refactor: change webserver logic code & Router library source

--- example/server.js
+++ example/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express()
 const PORT = 8000
 
 app.use(express.static(path.join(__dirname)))
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     try {
-        const filePath = path.join(__dirname, "\\index.html")
+        const filePath = path.join(__dirname, "index.html")
 
         res.status(200).sendFile(filePath)
     } catch (error) {
